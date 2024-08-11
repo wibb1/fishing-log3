8431ac3319ebc14f23524671b23f398424ceecf3
Will Campbell <62625260+wibb1@users.noreply.github.com>
2024-08-11 11:53:46 -0400

    renamed package and testing

--- fishing-log-backend/src/main/java/com/fishingLog/FishingLogApplication.java
+++ fishing-log-backend/src/main/java/com/fishingLog/FishingLogApplication.java
@@ -1,13 +1,11 @@
-package fishing_log;
+package com.fishingLog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FishingLogApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FishingLogApplication.class, args);
 	}
-
 }
--- fishing-log-backend/src/test/java/com/fishingLog/FishingLogApplicationTests.java
+++ fishing-log-backend/src/test/java/com/fishingLog/FishingLogApplicationTests.java
@@ -1,4 +1,4 @@
-package fishing_log;
+package com.fishingLog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
