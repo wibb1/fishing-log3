8c4e54752fa23a07a4ef50a97f281b71a29d91c0
Will Campbell <62625260+wibb1@users.noreply.github.com>
2024-09-08 20:32:20 -0400

    adding checks to save

--- fishing-log-backend/src/main/java/com/fishingLog/spring/service/AstrologicalService.java
+++ fishing-log-backend/src/main/java/com/fishingLog/spring/service/AstrologicalService.java
@@ -3,10 +3,12 @@ package com.fishingLog.spring.service;
 import com.fishingLog.spring.model.Astrological;
 import com.fishingLog.spring.repository.AstrologicalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 @Service
 public class AstrologicalService {
         @Autowired
@@ -20,7 +22,15 @@ public class AstrologicalService {
             return astrologicalRepository.findById(id);
         }
 
+        public Optional<Astrological> findEqualAstrological(Astrological a) {
+            return astrologicalRepository.findOne(Example.of(a));
+        }
+
         public Astrological saveAstrological(Astrological astrological) {
+            Optional<Astrological> equalAstrological = findEqualAstrological(astrological);
+            if (equalAstrological.isPresent()) {
+                return equalAstrological.get();
+            }
             return astrologicalRepository.save(astrological);
         }
 
