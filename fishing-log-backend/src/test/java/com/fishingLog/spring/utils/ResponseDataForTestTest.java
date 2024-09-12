404ce71a5ef58f75c0b81a667d284e1b4e10a504
Will Campbell <62625260+wibb1@users.noreply.github.com>
2024-09-11 20:09:52 -0400

    fix failing test

--- fishing-log-backend/src/test/java/com/fishingLog/spring/utils/ResponseDataForTestTest.java
+++ fishing-log-backend/src/test/java/com/fishingLog/spring/utils/ResponseDataForTestTest.java
@@ -70,16 +70,7 @@ public class ResponseDataForTestTest {
         ResponseDataForTest responseDataForTest = new ResponseDataForTest();
         String actualResponse = responseDataForTest.getWeatherDataString();
 
-        String expectedWeather = "{\"data\":[{\"astronomicalDawn\":\"2024-07-12T07:16:38+00:00\"," +
-                "\"astronomicalDusk\":\"2024-07-13T02:23:16+00:00\",\"civilDawn\":\"2024-07-12T08:48:00+00:00\"," +
-                "\"civilDusk\":\"2024-07-13T00:51:55+00:00\",\"moonFraction\":0.3267293520444956,\"" +
-                "moonPhase\":{\"closest\":{\"text\":\"First quarter\",\"time\":\"2024-07-13T15:55:00+00:00\",\"value\":0.25}," +
-                "\"current\":{\"text\":\"Waxing crescent\",\"time\":\"2024-07-12T00:00:00+00:00\",\"value\":0.19367802604339596}}," +
-                "\"moonrise\":\"2024-07-12T15:53:14+00:00\",\"moonset\":\"2024-07-12T03:26:21+00:00\",\"nauticalDawn\":" +
-                "\"2024-07-12T08:05:52+00:00\",\"nauticalDusk\":\"2024-07-13T01:34:03+00:00\",\"sunrise\":" +
-                "\"2024-07-12T09:21:12+00:00\",\"sunset\":\"2024-07-13T00:18:43+00:00\",\"time\":" +
-                "\"2024-07-12T00:00:00+00:00\"}],\"meta\":{\"cost\":1,\"dailyQuota\":50,\"lat\":41.6,\"lng\":-70.8," +
-                "\"requestCount\":12,\"start\":\"2024-07-12 00:00\"}}";
+        String expectedWeather = "{\"hours\":[{\"airTemperature\":{\"sg\":23.38},\"cloudCover\":{\"sg\":43.7},\"currentDirection\":{\"sg\":321.8},\"currentSpeed\":{\"sg\":0.06},\"gust\":{\"sg\":13.53},\"humidity\":{\"sg\":90.9},\"pressure\":{\"sg\":1022.44},\"seaLevel\":{\"sg\":-0.54},\"secondarySwellDirection\":{\"sg\":173.39},\"secondarySwellHeight\":{\"sg\":0.09},\"secondarySwellPeriod\":{\"sg\":8.71},\"swellDirection\":{\"sg\":175.11},\"swellHeight\":{\"sg\":0.18},\"swellPeriod\":{\"sg\":5.82},\"time\":\"2024-07-12T21:00:00+00:00\",\"visibility\":{\"sg\":24.13},\"waveDirection\":{\"sg\":195.18},\"waveHeight\":{\"sg\":0.76},\"wavePeriod\":{\"sg\":4.67},\"windDirection\":{\"sg\":211.35},\"windSpeed\":{\"sg\":7.16},\"windWaveDirection\":{\"sg\":203.34},\"windWaveHeight\":{\"sg\":0.76},\"windWavePeriod\":{\"sg\":5.36}}],\"meta\":{\"cost\":1,\"dailyQuota\":50,\"end\":\"2024-07-12 21:00\",\"lat\":41.6,\"lng\":-70.8,\"params\":[\"airTemperature\",\"pressure\",\"cloudCover\",\"currentDirection\",\"currentSpeed\",\"gust\",\"humidity\",\"seaLevel\",\"visibility\",\"windDirection\",\"windSpeed\",\"seaLevel\",\"swellDirection\",\"swellHeight\",\"swellPeriod\",\"secondarySwellDirection\",\"secondarySwellHeight\",\"secondarySwellPeriod\",\"waveDirection\",\"waveHeight\",\"wavePeriod\",\"windWaveDirection\",\"windWaveHeight\",\"windWavePeriod\"],\"requestCount\":10,\"source\":[\"sg\"],\"start\":\"2024-07-12 21:00\"}}";
         assertEquals(expectedWeather, actualResponse);
     }
 
