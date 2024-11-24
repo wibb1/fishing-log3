b18ddb4a7c3a4c101be33dd11c242a16d4515033
Will Campbell <62625260+wibb1@users.noreply.github.com>
2024-11-24 16:50:46 -0500

    Unit tests for AstrologicalService

---
+++ fishing-log-backend/src/test/java/com/fishingLog/spring/service/unit/AstrologicalServiceUnitTest.java
@@ -0,0 +1,17 @@
+package com.fishingLog.spring.service.unit;
+
+import com.fishingLog.FishingLogApplication;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = FishingLogApplication.class)
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("UnitTest")
+@Tag("unit")
+public class AstrologicalServiceUnitTest {
+}
