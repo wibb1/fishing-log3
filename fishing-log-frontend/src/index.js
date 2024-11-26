46bf28d84503111239fd6dd377fe2da7bdde71e4
Will Campbell <62625260+wibb1@users.noreply.github.com>
2024-11-26 18:12:22 -0500

    reinstall of create-react-app

--- fishing-log-frontend/src/index.js
+++ fishing-log-frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.css";
-import "./custom-css.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
